d01bb2b97d8e0fce747429a953371b72408d0629
Kavita <okavita@gmail.com>
2014-10-12 19:19:41 -0600

    second make_a_play_test passes

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,10 +1,18 @@
 class PlaysController < ApplicationController
   def index
+    @plays = Play.all
   end
 
   def new
+    @play = Play.new
   end
 
   def create
+    @play = Play.create(word: params[:play][:word])
+    if @play.valid?
+      redirect_to plays_path
+    else
+      render new_play_path
+    end
   end
 end
--- app/models/play.rb
+++ app/models/play.rb
@@ -1,4 +1,5 @@
 class Play < ActiveRecord::Base
+  validates :word, presence: true
 
   def letter_scores
     {"A"=>1, "B"=>3, "C"=>3, "D"=>2, "E"=>1, "F"=>4, "G"=>2, "H"=>4, "I"=>1, "J"=>8,
